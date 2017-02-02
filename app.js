9325fdacf0f44dc73a74a8535cfe4d791b444c1c
Dawn Monroe <deetriplett@gmail.com>
2017-02-02 12:23:05 -0500

    completed get profile function
    and added try/catch for errors

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//Function to print message to console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
 function getProfile(username) {
   try {
   // Connect to the API URL (https://teamtreehouse.com/username.json)
@@ -7,10 +13,27 @@ function getProfile(username) {
                           response.on('data', data => {
                             body += data.toString();
                           });
-     response.on('end', () => {
+
+                          response.on('end', () => {
                             try {
                             
                             // Parse the data
                             const profile = JSON.parse(body);                            
                             // Print the data
                             printMessage(username, profile.badges.length, profile.points.JavaScript);
+                          } catch (error) {
+                            printError(error);
+                          }
+                          });
+                          
+                          
+                      });
+//Error
+request.on('error', error => console.error('Problem with request : ${error.message}' ));
+} catch (error) {
+  printError(error);
+  }
+}
+
+const users = process.argv.slice(2);
+users.forEach(getProfile);
